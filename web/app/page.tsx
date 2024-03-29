899bed29e35f319caf095049702fd97f2e2439a2
thiago <371426+dooart@users.noreply.github.com>
2024-03-29 17:26:49 -0300

    Honk

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -5,8 +5,8 @@ import SendMessageForm from "@/components/send-message-form";
 import SoulMessage from "@/components/soul-message";
 import UserMessage from "@/components/user-message";
 import { useOnMount } from "@/lib/hooks/use-on-mount";
+import { Soul, said } from "@opensouls/engine";
 import { Fragment, useRef, useState } from "react";
-import { Soul, said } from "soul-engine/soul";
 
 export type ChatMessage =
   | {
@@ -21,6 +21,7 @@ export type ChatMessage =
 export default function Page() {
   const [messages, setMessages] = useState<ChatMessage[]>([]);
   const [isThinking, setIsThinking] = useState(false);
+  const audioRef = useRef<HTMLAudioElement>(null);
 
   const { soul, isConnected, reconnect } = useSoul({
     onNewMessage: async (stream: AsyncIterable<string>) => {
@@ -67,18 +68,33 @@ export default function Page() {
             <code> helps you write CODE!</code>
           </h2>
         </div>
-        {messages.length > 0 && (
+
+        <div className="flex gap-4">
+          <audio ref={audioRef} src="/honk.mp3" hidden></audio>
           <Button
             small
+            onClick={() => {
+              setTimeout(() => {
+                audioRef.current?.play();
+              }, 0);
+            }}
+            className="text-primary font-medium bg-secondary hover:underline"
+          >
+            HONK
+          </Button>
+
+          <Button
+            small
+            disabled={isConnected && messages.length === 0}
             onClick={() => {
               reconnect().catch(console.error);
               setMessages([]);
             }}
-            className="text-primary font-medium hover:underline"
+            className="text-primary font-medium [&:not(:disabled):hover]:underline"
           >
             Start over
           </Button>
-        )}
+        </div>
       </div>
       <div className="flex flex-col gap-6 pb-64">
         <SoulMessage content="hello! how can i help you today?" />
